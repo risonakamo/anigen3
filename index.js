e89302154801d75f2b2172609a2f3b3097c5f2c9
risonakamo <hmykanae@gmail.com>
2019-01-08 22:45:32 -0500

    more query work functions to work with query results

--- index.js
+++ index.js
@@ -2,5 +2,71 @@ window.onload=main;
 
 function main()
 {
+    anilistUserQuery("risona",(data)=>{
+        console.log(processAnlistDataShowType(data,"FALL",2018));
+    });
+}
 
+//make a media list request to anilist using a constructed query.
+//query in anilist show query.gql.
+//user: anilist user name to query
+//callback(object data): callback is given the full anilist api response object
+function anilistUserQuery(user,callback)
+{
+    var r=new XMLHttpRequest();
+    r.open("POST","https://graphql.anilist.co");
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            callback(JSON.parse(r.response));
+        }
+    };
+
+    r.setRequestHeader("content-type","application/json");
+    r.send(JSON.stringify({query:`{MediaListCollection(userName:"${user}",type:ANIME,sort:[ADDED_TIME_DESC]){lists{name,entries{media{title{romaji,native},startDate{year,month},season,coverImage{large},siteUrl,genres,format}}}}}`}));
+}
+
+//process FULL anilist data object.
+//filters by given SEASON and YEAR, and sorts into TYPES of the shows.
+//returns a TypeSortedShowsOutput from data defs gql
+function processAnlistDataShowType(data,season="WINTER",year=2019)
+{
+    if (!data)
+    {
+        return {};
+    }
+
+    data=data.data.MediaListCollection.lists;
+
+    var res={};
+    var shows;
+    var show;
+
+    //over all lists
+    for (var x=0,l=data.length;x<l;x++)
+    {
+        shows=data[x].entries;
+
+        //over all shows in a single list
+        for (var y=0,ly=shows.length;y<ly;y++)
+        {
+            show=shows[y].media;
+
+            if (show.season==season && show.startDate.year==year)
+            {
+                if (res[show.format])
+                {
+                    res[show.format].push(show);
+                }
+
+                else
+                {
+                    res[show.format]=[show];
+                }
+            }
+        }
+    }
+
+    return res;
 }
