119c990e3f38ef32194d7e82da3dd7c8d0a7fd81
risonakamo <hmykanae@gmail.com>
2019-01-10 15:15:54 -0500

    multiselector element

--- js/showmenu.js
+++ js/showmenu.js
@@ -9,13 +9,11 @@ class ShowMenu extends React.Component {
       className: "white-textbox"
     })), React.createElement("div", {
       className: "menu-block"
-    }, React.createElement("div", {
-      className: "left-text"
-    }, "\u5B63\u7BC0"), React.createElement("div", {
-      className: "white-multiselect"
-    }, React.createElement("span", {
-      className: "selected"
-    }, "\u6625"), React.createElement("span", null, "\u590F"), React.createElement("span", null, "\u79CB"), React.createElement("span", null, "\u51AC"))), React.createElement("div", {
+    }, React.createElement(WhiteMultiSelect, {
+      title: "\u5B63\u7BC0",
+      actualChoices: ["春", "夏", "秋", "冬"],
+      choices: ["SPRING", "SUMMER", "FALL", "WINTER"]
+    })), React.createElement("div", {
       className: "menu-block"
     }, React.createElement("div", {
       className: "left-text"
@@ -24,19 +22,53 @@ class ShowMenu extends React.Component {
       className: "white-textbox"
     })), React.createElement("div", {
       className: "menu-block"
-    }, React.createElement("div", {
-      className: "left-text"
-    }, "\u8A00\u8A9E"), React.createElement("div", {
-      className: "white-multiselect pill"
-    }, React.createElement("span", {
-      className: "selected"
-    }, "\u65E5\u672C\u8A9E"), React.createElement("span", null, "\u82F1\u8A9E"))), React.createElement("div", {
-      className: "menu-block double-top"
+    }, React.createElement(WhiteMultiSelect, {
+      title: "\u8A00\u8A9E",
+      actualChoices: ["日本語", "英語", "実際英語"],
+      choices: ["native", "romaji", "english"],
+      customClasses: "pill multi-line-wide",
+      leftTextClass: "top"
+    })), React.createElement("div", {
+      className: "menu-block"
     }, React.createElement("div", {
       className: "left-text"
     }), React.createElement("div", {
       className: "white-button green"
     }, "\u5B8C\u4E86")));
+  }
+
+}
+
+class WhiteMultiSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 0
+    };
+  }
+
+  render() {
+    return React.createElement(React.Fragment, null, React.createElement("div", {
+      className: `left-text ${this.props.leftTextClass}`
+    }, this.props.title), React.createElement("div", {
+      className: `white-multiselect ${this.props.customClasses}`
+    }, this.props.actualChoices.map((x, i) => {
+      var selectedClass = "";
+
+      if (this.state.selected == i) {
+        selectedClass = "selected";
+      }
+
+      return React.createElement("span", {
+        key: i,
+        className: selectedClass,
+        onClick: () => {
+          this.setState({
+            selected: i
+          });
+        }
+      }, x);
+    })));
   }
 
 }
--- js/showmenu.jsx
+++ js/showmenu.jsx
@@ -11,13 +11,9 @@ class ShowMenu extends React.Component
       </div>
 
       <div className="menu-block">
-        <div className="left-text">季節</div>
-        <div className="white-multiselect">
-          <span className="selected">春</span>
-          <span>夏</span>
-          <span>秋</span>
-          <span>冬</span>
-        </div>
+        <WhiteMultiSelect title="季節" actualChoices={["春","夏","秋","冬"]}
+          choices={["SPRING","SUMMER","FALL","WINTER"]}
+        />
       </div>
 
       <div className="menu-block">
@@ -26,16 +22,57 @@ class ShowMenu extends React.Component
       </div>
 
       <div className="menu-block">
-        <div className="left-text">言語</div>
-        <div className="white-multiselect pill">
-          <span className="selected">日本語</span>
-          <span>英語</span>
-        </div>
+        <WhiteMultiSelect title="言語" actualChoices={["日本語","英語","実際英語"]}
+          choices={["native","romaji","english"]} customClasses="pill multi-line-wide"
+          leftTextClass="top"
+        />
       </div>
 
-      <div className="menu-block double-top">
+      <div className="menu-block">
         <div className="left-text"></div>
         <div className="white-button green">完了</div>
+      </div>
+    </>;
+  }
+}
+
+/*a customisable "white multiselect" element
+  WhiteMultiselect(string-array actualChoices, string-array choices, string title, string customClasses)
+  actualChoices: string array of choices that will be displayed
+  choices: string array in equal size to actualChoices, corresponding to what choice
+           will be returned as selected. so the choice marked as selected can be
+           different from the displayed choice.
+  title: title to appear on the left side of the element
+  customClasses: custom class strings to add
+  leftTextClass: custom class for leftText*/
+class WhiteMultiSelect extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state={
+      selected:0 //which choice is selected
+    };
+  }
+
+  render()
+  {
+    return <>
+      <div className={`left-text ${this.props.leftTextClass}`}>{this.props.title}</div>
+      <div className={`white-multiselect ${this.props.customClasses}`}>
+        {this.props.actualChoices.map((x,i)=>{
+          var selectedClass="";
+
+          if (this.state.selected==i)
+          {
+            selectedClass="selected";
+          }
+
+          return <span key={i} className={selectedClass}
+            onClick={()=>{this.setState({selected:i})}}>{x}
+          </span>;
+        })}
       </div>
     </>;
   }
